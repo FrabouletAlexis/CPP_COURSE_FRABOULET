8a730517e66cd41ef8598d4fcf774b3eb69a691c
FrabouletAlexis <a.fraboulet@rns.ecolescreatives.com>
2021-03-10 16:57:33 +0100

    Update course.cpp

--- course.cpp
+++ course.cpp
@@ -10,7 +10,6 @@ using namespace std;
 
 /*void Course::moyen_temp(){};*/
 void Course::moyen_temp(){
-
     int moyen = (_score1+_score2+_score3)/3;
     std::cout<< moyen << std::endl;
 };
@@ -28,5 +27,25 @@ void Course::affiche_score(){
 };
 
 Course::Course(std::string pseudo, int _score1, int _score2, int _score3): _pseudo(pseudo), _score1(_score1), _score2(_score2), _score3(_score3){  
+    if(_score1 > 999){
+        _score1= 999;
+    };
+    else if (_score1 < -1){
+        _score1=0; /*on ne le compte pas*/
+    };
 
+    if(_score2 > 999){
+        _score2= 999;
+    };
+    else if (_score2 < -1){
+        _score2=0; /*on ne le compte pas*/
+    };
+
+    if(_score3 > 999){
+        _score3= 999;
+    };
+    else if (_score3 < -1){
+        _score3=0; /*on ne le compte pas*/
+    };
+    
 }
